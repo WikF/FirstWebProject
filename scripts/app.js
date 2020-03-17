7dd4a2a030ebf32add3b851e065b3cda45f60647
Wiktor <wikifed@gmail.com>
2020-03-17 17:33:59 +0100

    fixing editing list

--- scripts/app.js
+++ scripts/app.js
@@ -13,11 +13,58 @@ for (i = 0; i < mojaLista.length; i++) {
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
   }
 }
+
+
+//moja lista - dodawanie edycji 
+
+var mojaLista = document.getElementsByClassName("list");
+var i;
+for (i = 0; i < mojaLista.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("✎");
+  span.className = "edit";
+  span.appendChild(txt);
+  mojaLista[i].appendChild(span);
+}
+
+//funkcja edytowania
+function edit() {
+  var edit = document.getElementsByClassName("edit");
+  var i;
+  for (i = 0; i < edit.length; i++) {
+    edit[i].onclick = function () {
+      var div = this.parentElement;
+      var p = prompt("Edit your entry");
+      div.innerHTML = p;
+      var spanDel = document.createElement("SPAN");
+      var spanEdit = document.createElement("SPAN");
+      var txtEdit = document.createTextNode("✎");
+      var txtDel = document.createTextNode("\u00D7");
+      spanDel.className = "close";
+      spanEdit.className = "edit"
+      spanDel.appendChild(txtDel);
+      spanEdit.appendChild(txtEdit);
+      div.appendChild(spanDel);
+      div.appendChild(spanEdit);
+    }
+
+  }
+  var close = document.getElementsByClassName("close");
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+
+
 //dodawanie nowego elementu + okienko kiedy chcemy wpisać puste pole
 function newElement() {
   var li = document.createElement("li");
@@ -30,17 +77,47 @@ function newElement() {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
+  var spanDel = document.createElement("SPAN");
+  var spanEdit = document.createElement("SPAN");
+  var txtEdit = document.createTextNode("✎");
+  var txtDel = document.createTextNode("\u00D7");
+  spanDel.className = "close";
+  spanEdit.className = "edit"
   li.className = "list"
-  span.appendChild(txt);
-  li.appendChild(span);
+  spanDel.appendChild(txtDel);
+  spanEdit.appendChild(txtEdit);
+  li.appendChild(spanDel);
+  li.appendChild(spanEdit);
 
+  var edit = document.getElementsByClassName("edit");
+  var i;
+  for (i = 0; i < edit.length; i++) {
+    edit[i].onclick = function () {
+      var div = this.parentElement;
+      var p = prompt("Edit your entry");
+      div.innerHTML = p;
+      var spanDel = document.createElement("SPAN");
+      var spanEdit = document.createElement("SPAN");
+      var txtEdit = document.createTextNode("✎");
+      var txtDel = document.createTextNode("\u00D7");
+      spanDel.className = "close";
+      spanEdit.className = "edit"
+      spanDel.appendChild(txtDel);
+      spanEdit.appendChild(txtEdit);
+      div.appendChild(spanDel);
+      div.appendChild(spanEdit);
+    }
+
+  }
+  var close = document.getElementsByClassName("close");
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
+      li.appendChild(spanDel);
+      li.appendChild(spanEdit);
     }
   }
 }
+
+
